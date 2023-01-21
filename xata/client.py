dfb4449ea3bc9f7be685afa6cd3ceb98e72aa3dd
Philip Krauss <philip@philipkrauss.at>
2023-01-21 10:40:22 +0100

    added makefile

--- xata/client.py
+++ xata/client.py
@@ -105,9 +105,7 @@ class XataClient:
         self.branch_name = (
             self.get_branch_name_if_configured() if branch_name is None else branch_name
         )
-        self.headers = {
-            "authorization": f"Bearer {self.api_key}"
-        }
+        self.headers = {"authorization": f"Bearer {self.api_key}"}
 
         # print (
         #   f"API key: {self.api_key}, "
@@ -174,7 +172,10 @@ class XataClient:
         return os.environ.get("XATA_BRANCH")
 
     def request(self, method, urlPath, cp=False, headers={}, expect_codes=[], **kwargs):
-        headers = {**headers, **self.headers} # TODO use "|" when client py min version >= 3.9
+        headers = {
+            **headers,
+            **self.headers,
+        }  # TODO use "|" when client py min version >= 3.9
         base_url = self.base_url if not cp else self.control_plane_url
         url = urljoin(base_url, urlPath.lstrip("/"))
         resp = requests.request(method, url, headers=headers, **kwargs)
