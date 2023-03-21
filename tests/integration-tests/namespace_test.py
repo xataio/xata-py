c741c008cffd75d90dc19f407980c49b32718ac1
Philip Krauss <philip@philipkrauss.at>
2023-03-21 15:48:39 +0100

    investigate direct namespace invocation

---
+++ tests/integration-tests/namespace_test.py
@@ -0,0 +1,54 @@
+#
+# Licensed to Xatabase, Inc under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Xatabase, Inc licenses this file to you under the
+# Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You
+# may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import utils
+
+from xata.client import XataClient
+
+
+class TestNamespace(object):
+    def test_direct_instance_in_namespace_without_client_dependencies(self):
+        """
+        Direct namespace invocation without implicit config
+        :link https://github.com/xataio/xata-py/issues/57
+        """
+        users = XataClient().users()
+        r1 = users.getUser()
+        assert r1.status_code == 200
+        
+        client = XataClient()
+        r2 = client.users().getUser()
+        assert r2.status_code == 200
+
+        assert r1.json() == r2.json()
+
+    def test_direct_instance_in_namespace_wit_client_dependencies(self):
+        """
+        Direct namespace invocation with dependency on internal client config
+        :link https://github.com/xataio/xata-py/issues/57
+        """
+        db_name = utils.get_db_name()
+        databases = XataClient().databases()
+
+        # workspace_id should be provided by the the client implicitly
+        assert databases.createDatabase(db_name, {"region": "eu-west-1"}).status_code == 201
+        assert databases.getDatabaseMetadata(db_name).status_code == 200
+        assert databases.deleteDatabase(db_name).status_code == 200
+
+
