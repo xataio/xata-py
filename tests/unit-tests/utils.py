dd813eb533449b20bbf3eac839e8d205af8a3ef4
Philip Krauss <35487337+philkra@users.noreply.github.com>
2023-05-05 20:42:22 +0200

    Prepare 1.0.0.a1 release (#90)
    * bump release number and dependencies
    
    * extend regex for preview versions

--- tests/unit-tests/utils.py
+++ tests/unit-tests/utils.py
@@ -20,4 +20,4 @@
 import re
 
 PATTERNS_UUID4 = re.compile(r"^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$", re.IGNORECASE)
-PATTERNS_SDK_VERSION = re.compile(r"^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$")
+PATTERNS_SDK_VERSION = re.compile(r"^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}(.[ab][0-9]{1,3})*$")
--- xata/client.py
+++ xata/client.py
@@ -37,7 +37,7 @@ from .namespaces.workspace.table import Table
 
 # TODO this is a manual task, to keep in sync with pyproject.toml
 # could/should be automated to keep in sync
-__version__ = "0.10.1"
+__version__ = "1.0.0.a1"
 
 PERSONAL_API_KEY_LOCATION = "~/.config/xata/key"
 DEFAULT_DATA_PLANE_DOMAIN = "xata.sh"
