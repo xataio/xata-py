71966c1ccc1801d427869177b31c7be886f7746f
Philip Krauss <philip@philipkrauss.at>
2023-07-04 20:14:44 +0200

    RTD run

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = "sphinx_rtd_theme"
-# html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+#html_static_path = ['_static']
 
 sys.path.insert(0, os.path.abspath(".."))
