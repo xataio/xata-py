60f758a45dddca949d7c3eefcd72f1e23853a3aa
Philip Krauss <philip@philipkrauss.at>
2023-07-26 12:47:10 +0200

    Add revlinks integration tests

--- tests/integration-tests/search_and_filter_revlinks_test.py
+++ tests/integration-tests/search_and_filter_revlinks_test.py
@@ -17,8 +17,9 @@
 # under the License.
 #
 
-import utils
 import random
+
+import utils
 from faker import Faker
 
 from xata.client import XataClient
@@ -31,12 +32,12 @@ class TestSearchAndFilterRevLinks(object):
         self.record_id = utils.get_random_string(24)
         self.client = XataClient(db_name=self.db_name)
 
-        assert self.client.databases().create(self.db_name).is_success()
+        assert self.client.databases().createDatabase(self.db_name, {"region": "us-east-1"}).status_code == 201
 
-        assert self.client.table().create("Users").is_success()
-        assert self.client.table().create("Posts").is_success()
+        assert self.client.table().createTable("Users").status_code == 201
+        assert self.client.table().createTable("Posts").status_code == 201
 
-        r = self.client.table().set_schema(
+        r = self.client.table().setTableSchema(
             "Users",
             {
                 "columns": [
@@ -45,8 +46,8 @@ class TestSearchAndFilterRevLinks(object):
                 ]
             },
         )
-        assert r.is_success()
-        r = self.client.table().set_schema(
+        assert r.status_code == 200
+        r = self.client.table().setTableSchema(
             "Posts",
             {
                 "columns": [
@@ -56,7 +57,7 @@ class TestSearchAndFilterRevLinks(object):
                 ]
             },
         )
-        assert r.is_success()
+        assert r.status_code == 200
 
         ids = [self.fake.isbn13() for i in range(10)]
         self.users = [
@@ -67,7 +68,7 @@ class TestSearchAndFilterRevLinks(object):
             }
             for i in range(len(ids))
         ]
-        assert self.client.records().bulk_insert("Users", {"records": self.users}).is_success()
+        assert self.client.records().bulkInsertTableRecords("Users", {"records": self.users}).status_code == 200
         self.posts = [
             {
                 "title": self.fake.company(),
@@ -76,35 +77,35 @@ class TestSearchAndFilterRevLinks(object):
             }
             for i in range(50)
         ]
-        assert self.client.records().bulk_insert("Posts", {"records": self.posts}).is_success()
+        assert self.client.records().bulkInsertTableRecords("Posts", {"records": self.posts}).status_code == 200
 
     def teardown_class(self):
-        assert self.client.databases().delete(self.db_name).is_success()
+        assert self.client.databases().deleteDatabase(self.db_name).status_code == 200
 
     def test_revlinks_with_alias(self):
         payload = {
             "columns": [
-            "name",
+                "name",
                 {
                     "name": "<-Posts.author",
                     "columns": ["title"],
                     "as": "posts",
                     "limit": 5,
                     "offset": 0,
-                    "order": [{"column": "createdAt", "order": "desc"}]
-                }
+                    "order": [{"column": "createdAt", "order": "desc"}],
+                },
             ],
             "page": {"size": 5},
         }
-        r = self.client.data().query("Users", payload)
-        assert r.is_success()
-        assert "records" in r
-        assert len(r["records"]) == 5
-        assert "name" in r["records"][0]
-        assert "posts" in r["records"][0]
-        assert "records" in r["records"][0]["posts"]
+        r = self.client.data().queryTable("Users", payload)
+        assert r.status_code == 200
+        assert "records" in r.json()
+        assert len(r.json()["records"]) == 5
+        assert "name" in r.json()["records"][0]
+        assert "posts" in r.json()["records"][0]
+        assert "records" in r.json()["records"][0]["posts"]
 
-        posts = r["records"][0]["posts"]["records"]
+        posts = r.json()["records"][0]["posts"]["records"]
         assert len(posts) >= 1
         assert len(posts) <= 5
 
@@ -114,36 +115,22 @@ class TestSearchAndFilterRevLinks(object):
 
     def test_revlinks_without_alias(self):
         payload = {
-            "columns": [
-            "name",
-                {
-                    "name": "<-Posts.author",
-                    "columns": ["title"]
-                }
-            ],
+            "columns": ["name", {"name": "<-Posts.author", "columns": ["title"]}],
             "page": {"size": 1},
         }
-        r = self.client.data().query("Users", payload)
-        assert r.is_success()
-        assert "records" in r
-        assert "name" in r["records"][0]
-        assert "Postsauthor" in r["records"][0]
-        assert "records" in r["records"][0]["Postsauthor"]
+        r = self.client.data().queryTable("Users", payload)
+        assert r.status_code == 200
+        assert "records" in r.json()
+        assert "name" in r.json()["records"][0]
+        assert "Postsauthor" in r.json()["records"][0]
+        assert "records" in r.json()["records"][0]["Postsauthor"]
 
     def test_revlinks_with_limit_control(self):
         payload = {
-            "columns": [
-            "name",
-                {
-                    "name": "<-Posts.author",
-                    "columns": ["title"],
-                    "as": "posts",
-                    "limit": 1
-                }
-            ],
+            "columns": ["name", {"name": "<-Posts.author", "columns": ["title"], "as": "posts", "limit": 1}],
             "page": {"size": 5},
         }
-        r = self.client.data().query("Users", payload)
-        assert r.is_success()
-        posts = r["records"][0]["posts"]["records"]
+        r = self.client.data().queryTable("Users", payload)
+        assert r.status_code == 200
+        posts = r.json()["records"][0]["posts"]["records"]
         assert len(posts) == 1
