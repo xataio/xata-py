431c985785d150f6b336fe4f31eafb5728b1c964
Philip Krauss <philip@philipkrauss.at>
2023-07-27 14:24:48 +0200

    reworked api design

--- tests/integration-tests/search_and_filter_ask_table_test.py
+++ tests/integration-tests/search_and_filter_ask_table_test.py
@@ -17,47 +17,90 @@
 # under the License.
 #
 
-import utils
+from dotenv import dotenv_values
 
 from xata.client import XataClient
 
 
 class TestSearchAndFilterAskTableEndpoint(object):
-    """
-    POST /db/{db_branch_name}/tables/{table_name}/ask
-    """
-
     def setup_class(self):
-        self.db_name = utils.get_db_name()
-        self.branch_name = "main"
-        self.client = XataClient(db_name=self.db_name, branch_name=self.branch_name)
+        env = dotenv_values(".env")
+        self.client = XataClient(
+            workspace_id=env.get("XATA_TEST_ASK_WORKSPACE"), db_name=env.get("XATA_TEST_ASK_DATABASE")
+        )
 
-        assert self.client.databases().create(self.db_name).is_success()
-        assert self.client.table().create("Posts").is_success()
+    def test_ask_table_for_response_shape(self):
+        answer = self.client.data().ask("xata", "does xata have a python sdk")
+        assert answer.is_success()
 
-    def teardown_class(self):
-        assert self.client.databases().delete(self.db_name).is_success()
+        assert "answer" in answer
+        assert "records" in answer
+        assert "sessionId" in answer
 
-    def test_ask_table_for_response_shape_and_empty_response(self):
-        # TODO
-        # Use one call to
-        # - assert response shape
-        # - empty results
-        # - default response content-type
-        # payload = {"question": "how much is the fish?"}
-        # r = self.client.search_and_filter().askTable("Posts", payload)
-        # assert r.is_success()
-        # assert "answer" in r
-        # assert r["answer"] == "No records found! I'm not able to help you sorry."
-        # assert "content-type" in r.headers
-        # assert r.headers["content-type"] == "application/json; charset=UTF-8"
-        assert True
+        assert answer["answer"] is not None
+        assert answer["sessionId"] is not None
+        assert len(answer["records"]) > 0
 
-    def test_ask_table_with_streaming_response_content_type(self):
-        # TODO
-        # payload = {"question": "how much is the fish?"}
-        # r = self.client.search_and_filter().askTable("Posts", payload, response_content_type="text/event-stream")
-        # assert r.is_success()
-        # assert "content-type" in r.headers
-        # assert r.headers["content-type"] == "text/event-stream; charset=UTF-8"
+        assert answer.headers["content-type"].lower().startswith("application/json")
+
+    def test_ask_table_for_response_shape_with_rules(self):
+        answer = self.client.data().ask("xata", "what database technology is used at xata", ["postgres is a database"])
+        assert answer.is_success()
+
+        assert "answer" in answer
+        assert "records" in answer
+        assert "sessionId" in answer
+
+        assert answer["answer"] is not None
+        assert answer["sessionId"] is not None
+        assert len(answer["records"]) > 0
+
+    def test_ask_table_for_response_shape_with_options(self):
+        opts = {
+            "searchType": "keyword",
+            "search": {
+                "fuzziness": 1,
+                "prefix": "phrase",
+                "target": [
+                    "slug",
+                    {"column": "title", "weight": 4},
+                    "content",
+                    "section",
+                    {"column": "keywords", "weight": 4},
+                ],
+                "boosters": [
+                    {
+                        "valueBooster": {
+                            "column": "section",
+                            "value": "guide",
+                            "factor": 18,
+                        },
+                    },
+                ],
+            },
+        }
+        answer = self.client.data().ask("xata", "how to do full text search?", options=opts)
+        assert answer.is_success()
+
+        assert "answer" in answer
+        assert "records" in answer
+        assert "sessionId" in answer
+
+        assert answer["answer"] is not None
+        assert answer["sessionId"] is not None
+        assert len(answer["records"]) > 0
+
+    def test_ask_table_with_streaming_response(self):
+        answer = self.client.data().ask("xata", "does the data model have link type?", streaming_results=True)
+        assert answer.is_success()
+
+        #assert "answer" in answer
+        #assert "records" in answer
+        #assert "sessionId" in answer
+
+        #assert answer["answer"] is not None
+        #assert answer["sessionId"] is not None
+        #assert len(answer["records"]) > 0
+
+        #assert answer.headers["content-type"].lower().startswith("text/event-stream")
         assert True
