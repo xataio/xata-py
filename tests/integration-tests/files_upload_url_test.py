8fc6437ae53ec1f66151ed2192be14cbf771d923
Philip Krauss <philip@philipkrauss.at>
2024-05-28 13:54:39 +0200

    adjust tests after dc tests

--- tests/integration-tests/files_upload_url_test.py
+++ tests/integration-tests/files_upload_url_test.py
@@ -19,24 +19,16 @@
 
 import utils
 
-import logging
-
 from requests import request
 from xata.client import XataClient
-
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
 
 class TestFilesSingleFile(object):
     def setup_class(self):
         self.db_name = utils.get_db_name()
-        self.db_name = "Testing-DC-from-Philip"
-
         self.branch_name = "main"
         self.client = XataClient(db_name=self.db_name, branch_name=self.branch_name)
         self.fake = utils.get_faker()
 
-        """
         assert self.client.databases().create(self.db_name).is_success()
         assert self.client.table().create("Attachments").is_success()
         assert (
@@ -49,11 +41,9 @@ class TestFilesSingleFile(object):
             )
             .is_success()
         )
-        """
 
     def teardown_class(self):
-        #assert self.client.databases().delete(self.db_name).is_success()
-        pass
+        assert self.client.databases().delete(self.db_name).is_success()
 
     def test_upload_file(self):
         payload = {"title": self.fake.catch_phrase()}
@@ -64,7 +54,6 @@ class TestFilesSingleFile(object):
         gif = utils.get_file_content(utils.get_file_name("images/01.gif"))
 
         resp = self.client.files().put("Attachments", rid, "one_file", gif)
-        assert resp.status_code == ""
         assert resp.is_success()
 
         record = self.client.records().get("Attachments", rid, columns=["one_file.*", "one_file.uploadUrl"])
